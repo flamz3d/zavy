f02ab2407191acc618ad26e10dd8bead6bbdcf46
dom <dom@unity3d.com>
2018-11-27 12:03:07 -0500

    port 80

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var SpellChecker = require('spellchecker');
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 80
 app.use(express.static('public'))
 
 function shuffle(array) {
