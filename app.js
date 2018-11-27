032acab49cfa2ffda99884e1b3082fa9dd2d1e7a
dom <dom@unity3d.com>
2018-11-27 12:06:26 -0500

    use port env var

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var SpellChecker = require('spellchecker');
 
 const express = require('express')
 const app = express()
-const port = 80
+const port = process.env.PORT || 3000;
 app.use(express.static('public'))
 
 function shuffle(array) {
